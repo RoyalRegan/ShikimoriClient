48ddeb33bd27132a2d2a974568fea5b911c20098
Vlad <jaju2251@yandex.ru>
2019-05-28 01:17:02 +0300

    Add Publisher for manga and ranobe

--- app/src/main/java/com/example/shikimoriclient/BackEnd/dao/manga/Publisher.java
+++ app/src/main/java/com/example/shikimoriclient/BackEnd/dao/manga/Publisher.java
@@ -1,4 +1,14 @@
 package com.example.shikimoriclient.BackEnd.dao.manga;
 
 public class Publisher {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
