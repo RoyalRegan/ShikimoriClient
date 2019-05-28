43eb54a230a9d279876f6c1c8c361bba511e140e
Marina <marina-cheprakova@mail.ru>
2019-05-29 01:11:42 +0300

    Add menu on info page

--- app/src/main/java/com/example/shikimoriclient/DetailInfo.java
+++ app/src/main/java/com/example/shikimoriclient/DetailInfo.java
@@ -3,14 +3,17 @@ package com.example.shikimoriclient;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.MenuBuilder;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.support.v4.app.FragmentManager;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.PopupMenu;
 
 import com.example.shikimoriclient.BackEnd.dao.ItemSimple;
 import com.example.shikimoriclient.BackEnd.dao.anime.Anime;
@@ -19,9 +22,10 @@ import com.example.shikimoriclient.FrontEnd.fragments.AnimeDetails;
 import com.example.shikimoriclient.FrontEnd.fragments.RanobeOrMangaDetails;
 
 //TODO: Add similar(as context dialog like RecyclerViewFragment) and related(?)
-public class DetailInfo extends AppCompatActivity {
+public class DetailInfo extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener{
     private Toolbar toolbar;
     private CollapsingToolbarLayout collapsingToolbarLayout;
+    private FloatingActionButton floatingActionButton;
     private FragmentManager myFragmentManager;
     private FragmentTransaction fragmentTransaction;
     private boolean animeType;
@@ -37,6 +41,7 @@ public class DetailInfo extends AppCompatActivity {
     private void initializeComp() {
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        floatingActionButton = findViewById(R.id.menuButton);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         collapsingToolbarLayout = findViewById(R.id.collapsing_toolbar);
         myFragmentManager = getSupportFragmentManager();
@@ -72,21 +77,34 @@ public class DetailInfo extends AppCompatActivity {
 
     private void setListeners() {
 
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popup = new PopupMenu(DetailInfo.this, v);
+                popup.setOnMenuItemClickListener(DetailInfo.this);
+                popup.inflate(R.menu.menu_anime_details);
+                popup.show();
+            }
+        });
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        if (animeType) {
-            getMenuInflater().inflate(R.menu.menu_anime_details, menu);
-        } else {
-            getMenuInflater().inflate(R.menu.menu_ranobe_and_manga_details, menu);
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        MenuInflater inflater = popup.getMenuInflater();
+        if (animeType)
+        {
+            inflater.inflate(R.menu.menu_anime_details, popup.getMenu());
         }
-        return true;
+        else
+        {
+            inflater.inflate(R.menu.menu_ranobe_and_manga_details, popup.getMenu());
+        }
+        popup.show();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onMenuItemClick(MenuItem item) {
         if (animeType) {
             switch (item.getItemId()) {
                 case R.id.watch_list:
